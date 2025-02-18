0fca64e113f447b2e5ca1e363bfc59a27e39ff9f
Remi Oyediji <remioyediji@Remis-MacBook-Pro.local>
2025-02-18 11:30:03 -0700

    automated server retrieval

--- scripts/start_mlflow.sh
+++ scripts/start_mlflow.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Starting MLflow Server..."
-docker-compose up -d
+# Fetch the public IP of the server
+SERVER_IP=$(hostname -I | awk '{print $1}')
 
-echo "MLflow Server is running! Access it at: http://your-server-ip:5000"
+echo "Starting MLflow Server..."
+sudo docker-compose up -d
+
+echo "MLflow Server is running at: http://$SERVER_IP:5000"
