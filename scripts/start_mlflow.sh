c0ca389ca5639be4f6b3cf5c6f2a90647defbb4a
Remi Oyediji <remioyediji@Remis-MacBook-Pro.local>
2025-02-18 22:37:09 -0700

    updated region

--- scripts/start_mlflow.sh
+++ scripts/start_mlflow.sh
@@ -7,3 +7,8 @@ echo "Starting MLflow Server..."
 sudo docker-compose up -d
 
 echo "MLflow Server is running at: http://$SERVER_IP:5000"
+
+# Ensure firewall allow port 5000
+#sudo ufw allow 5000/tcp
+#sudo ufw enable
+#sudo ufw status
